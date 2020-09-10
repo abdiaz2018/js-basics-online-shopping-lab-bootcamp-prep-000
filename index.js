eeb293fbea7b93692c4c4e7424bbac551a59322a
Abraham Diaz <abdiaz2018@gmail.com>
2020-09-10 23:40:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,6 +61,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-
+if (!cardNumber) {
+  return "Sorry, we don't have a credit card on file for you."
+}
   // end it here
 }
