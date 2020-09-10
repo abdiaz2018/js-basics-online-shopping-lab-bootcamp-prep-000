3ddd02d4e1f4ecdac4eef322809f589f4d3c9331
Abraham Diaz <abdiaz2018@gmail.com>
2020-09-10 20:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,20 +11,46 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-}
+ var itemPrice = Math.floor(Math.random() * (100) + 1);
+ var itemObj = {itemPrice: itemPrice, itemName: item};
+ cart.push(itemObj);
+ return (`${item} has been added to your cart.`)
+ return cart
+ }
+ // end it here
+
 
 function viewCart() {
   // write your code here
+  if(cart.length === 0) {
+    return ('Your shopping cart is empty.')
+  }
+  else if(cart.length ===1) {
+    return (`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`)
+  }
+  else if(cart.length ===2) {
+    return(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`)
+  }
+  else if(cart.length > 2) {
+    return(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`)
+  }
+  // end it here
 }
 
 function total() {
   // write your code here
+
+  // end it here
 }
 
 function removeFromCart(item) {
   // write your code here
+
+  // end it here
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+
+  // end it here
 }
