95fed3f0b86470cff90014dc994cf8fcf30852e4
Abraham Diaz <abdiaz2018@gmail.com>
2020-09-10 22:12:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function viewCart() {
 function total() {
   // write your code here
   var total = 0
-  for (let i = 0; i < cart.lenghth; total+itemPrice=)
+  for (let i = 0; i < cart.lenghth; total+=itemPrice)
 return total
   // end it here
 }
