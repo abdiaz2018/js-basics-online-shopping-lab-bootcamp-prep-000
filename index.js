8dd4417b03e27e113e88159af91cd5e0d8829bcc
Abraham Diaz <abdiaz2018@gmail.com>
2020-09-11 00:45:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ if (!cardNumber) {
 }
 else if(cardNumber) {
   return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
-  function setCart([]) {
-  cart = [];
+  function setCart(c) {
+  cart = c;
   return cart;
 }
     }
