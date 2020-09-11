af00b857fc0b5030751fb8f383cf5f32a564d31e
Abraham Diaz <abdiaz2018@gmail.com>
2020-09-11 00:01:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ if (!cardNumber) {
   return "Sorry, we don't have a credit card on file for you."
 }
 else if(cardNumber) {
-  return `Your total cost is $${}, which will be charged to the card ${cardNumber}.`
+  return `Your total cost is $${cart}, which will be charged to the card ${cardNumber}.`
 }
   // end it here
 }
